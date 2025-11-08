91ba91f356fdd9e9e00cddf3aa68cdc7333ca9e8
Yona Paul <ypaul6764@gmail.com>
2025-11-08 19:15:22 +0300

    First Lesson Variables

---
+++ Code/App1/Lessons/Variables.cs
@@ -0,0 +1,34 @@
+namespace Lessons;
+
+class Variables
+// Primitives and Constants.
+{
+    public static void Run()
+    {
+
+        // Variables are mutable while constants are not.
+        // Variables can be declared without initialization.
+        // Constants must be initialized at the time of declaration.
+
+        // Rules for identifiers: 
+        // - Must start with a letter or underscore
+        // - Cannot start with a number
+        // - Can contain letters, numbers, and underscores
+        // - Cannot be a reserved keyword
+        // - Always use meaningful names
+        // Notations include: camelCase, PascalCase, snake_case, strHungarianNotation
+        int myNum = 5;               // Integer (whole number)
+        double myDoubleNum = 5.99;  // Floating point number
+        char myLetter = 'D';        // Character
+        string myText = "Hello";    // String
+        bool myBool = true;         // Boolean
+
+        const float Pi = 3.14f; // Constant variable (Use PascalCase for constants)
+
+        Console.WriteLine("Integer: " + myNum);
+        Console.WriteLine("Double: " + myDoubleNum);
+        Console.WriteLine("Character: " + myLetter);
+        Console.WriteLine("String: " + myText);
+        Console.WriteLine("Boolean: " + myBool);
+    }
+}
---
+++ Code/App1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Lessons.Variables.Run();
+    }
+}
+
